66aaf2b6ab65e70c9588554be89c90b897703ec2
mohammed067 <mohammedrahaman596@gmail.com>
2024-03-01 11:52:05 +0530

    cd_5 integration

--- src/component/layout/index.jsx
+++ src/component/layout/index.jsx
@@ -1,6 +1,5 @@
 import Sidebar from "./sidebar/index";
 import SidebarV2 from "./sidebar/SidebarV2";
-import HeaderOne from "./header/HeaderOne";
 import HeaderTwo from "./header/HeaderTwo";
 import { useState } from "react";
 import { createContext } from "react";
@@ -30,7 +29,6 @@ function Layout() {
           <Sidebar handleActive={() => setSidebar(!sidebar)} />
           <SidebarV2 />
           <div className={`body-wrapper flex-1 overflow-x-hidden`}>
-            <HeaderOne handleSidebar={() => setSidebar(!sidebar)} />
             <HeaderTwo handleSidebar={() => setSidebar(!sidebar)} />
             <Integrations />
           </div>
